c85c2217589dd261ee99e2060e7ef1c2abc63f43
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-04-19 15:22:48 -0500

    NativeSqlScalarQuery updarted to hibernarte 5

---
+++ NativeSqlScalarQuery/src/main/java/com/adarsh/ApplicationMain.java
@@ -0,0 +1,12 @@
+package com.adarsh;
+
+
+import static com.adarsh.service.EmployeeService.*;
+
+public class ApplicationMain {
+
+    public static void main(String[] args) throws Exception {
+        insertEmployee();
+        displayEmployee();
+    }
+}
--- NativeSqlScalarQuery/src/main/java/com/adarsh/factory/MySessionFactory.java
+++ NativeSqlScalarQuery/src/main/java/com/adarsh/factory/MySessionFactory.java
@@ -2,16 +2,17 @@ package com.adarsh.factory;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.*;
+
 public class MySessionFactory {
-     
-	    private static SessionFactory sessionFactoryObject=null;
-	    
-	    static{
-	    	sessionFactoryObject=new Configuration().configure("hibernate-cfg.xml").buildSessionFactory();
-	    }
-	    
-	    public static SessionFactory getSessionFactory(){
-	    	return sessionFactoryObject;
-	    }
-	
+
+    private static SessionFactory sessionFactoryObject = null;
+
+    static {
+        sessionFactoryObject = new Configuration().configure("hibernate-cfg.xml").buildSessionFactory();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactoryObject;
+    }
+
 }
