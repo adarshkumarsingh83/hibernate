b0bbcd3929af867a2bfd833b47851f52b15a752f
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-04-19 14:26:49 -0500

    HibernateIndexApplication updated to hibernate 5

---
+++ HibernateIndexApplication/src/main/java/com/adarsh/entity/Employee.java
@@ -0,0 +1,90 @@
+package com.adarsh.entity;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+
+@Slf4j
+public class Employee implements Serializable, Cloneable {
+
+
+    private static final long serialVersionUID = 1L;
+    private Integer empNumber;
+    private String empFirstName;
+    private String empMiddleName;
+    private String empLastName;
+    private String empCity;
+    private Date dateOfBirth;
+
+    static {
+        log.info("Static block of the employee");
+    }
+
+    public Employee() {
+        log.info("Employee Constructor");
+    }
+
+    public Integer getEmpNumber() {
+        return empNumber;
+    }
+
+    public void setEmpNumber(Integer empNumber) {
+        this.empNumber = empNumber;
+    }
+
+    public String getEmpFirstName() {
+        return empFirstName;
+    }
+
+    public void setEmpFirstName(String empFirstName) {
+        this.empFirstName = empFirstName;
+    }
+
+    public String getEmpMiddleName() {
+        return empMiddleName;
+    }
+
+    public void setEmpMiddleName(String empMiddleName) {
+        this.empMiddleName = empMiddleName;
+    }
+
+    public String getEmpLastName() {
+        return empLastName;
+    }
+
+    public void setEmpLastName(String empLastName) {
+        this.empLastName = empLastName;
+    }
+
+    public String getEmpCity() {
+        return empCity;
+    }
+
+    public void setEmpCity(String empCity) {
+        this.empCity = empCity;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Object clone() throws CloneNotSupportedException {
+        /* log.info("Employee Object is Cloned ");*/
+        Employee newEmpObject = (Employee) super.clone();
+        return newEmpObject;
+    }
+
+    public String toString() {
+        return this.empNumber + " " + this.empFirstName + " " + this.empMiddleName + " " + this.empLastName + " " + this.empCity;
+    }
+
+    public void displayEmployee() {
+        log.info("\nEmployee Number :" + this.empNumber + "\nEmployee Name: " + this.empFirstName + " " + this.empMiddleName + " " + this.empLastName + " " + this.empCity + " " + this.dateOfBirth);
+    }
+}
---
+++ HibernateIndexApplication/src/main/java/com/adarsh/service/EmployeeService.java
@@ -0,0 +1,76 @@
+package com.adarsh.service;
+
+import com.adarsh.entity.Employee;
+import com.adarsh.factory.MySessionFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Slf4j
+public class EmployeeService {
+
+    public static void insertEmployee() throws Exception {
+        Session sessionObject = MySessionFactory.getSessionFactory().openSession();
+        Transaction transactionObject = sessionObject.beginTransaction();
+        List<Employee> empList = getEmployee();
+        for (int i = 0; i < empList.size(); i++) {
+            sessionObject.saveOrUpdate(((Employee) empList.get(i)));
+        }
+        transactionObject.commit();
+        sessionObject.close();
+    }
+
+    public static List<Employee> getEmployee() throws Exception {
+        Employee empObject = new Employee();
+        List<Employee> empList = new ArrayList<Employee>();
+        Integer empNumber[] = {101, 102, 103, 104, 105};
+        String empName[] = {"adarsh", "amit", "abhishek", "amritesh", "ashok"};
+        String empCity[] = {"delhi", "bangalore", "pune", "hyderabad", "allahabad"};
+        String dateOfBirth[] = {"09/13/1983", "01/04/1986", "01/05/1986", "02/25/1983", "05/03/1984"};
+        for (int i = 0; i < 5; i++) {
+            Employee emp = (Employee) empObject.clone();
+            emp.setEmpNumber(empNumber[i]);
+            emp.setEmpFirstName(empName[i]);
+            emp.setEmpMiddleName("Kumar");
+            emp.setEmpLastName("Singh");
+            emp.setEmpCity(empCity[i]);
+            emp.setDateOfBirth(getDob(dateOfBirth[i]));
+            empList.add(emp);
+            emp = null;
+        }
+        return empList;
+    }
+
+    public static void displayEmployee() {
+        Integer empNumber[] = {101, 102, 103, 104, 105};
+        try {
+            Session sessionObject = MySessionFactory.getSessionFactory().openSession();
+            for (int i = 0; i < 5; i++) {
+                Employee emp = (Employee) sessionObject.get(Employee.class, empNumber[i]);
+                emp.displayEmployee();
+                emp = null;
+            }
+            sessionObject.close();
+        } catch (Exception exceptionObject) {
+            exceptionObject.getMessage();
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    public static java.sql.Date getDob(String dateOfBirth) {
+        java.sql.Date dataOfBirthObject = null;
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+            Date utilDate = formatter.parse(dateOfBirth);
+            dataOfBirthObject = new java.sql.Date(utilDate.getTime());
+        } catch (Exception exceptionObject) {
+            log.error(exceptionObject.getMessage());
+        }
+        return dataOfBirthObject;
+    }
+}
