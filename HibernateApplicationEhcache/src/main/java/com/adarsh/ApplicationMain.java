a08c942f90f8c632062ac4eecb271ffe5059a3e3
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-05-10 14:05:42 -0500

    HibernateApplicationEhcache updated to the hibernate 5

--- HibernateApplicationEhcache/src/main/java/com/adarsh/ApplicationMain.java
+++ HibernateApplicationEhcache/src/main/java/com/adarsh/ApplicationMain.java
@@ -1,4 +1,4 @@
-package com.adarsh.main;
+package com.adarsh;
 
 import com.adarsh.domain.Employee;
 import com.adarsh.factory.MySessionFactory;
@@ -7,18 +7,18 @@ import org.hibernate.Transaction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Application {
+public class ApplicationMain {
 
-    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+    private static final Logger logger = LoggerFactory.getLogger(ApplicationMain.class);
 
     public static void main(String[] args) {
 
         logger.info("Hibernate + MySQL");
         Session session = MySessionFactory.getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
-        Employee employee = new Employee(101, "adarsh", "adarsh@kumar", "IT");
+        Employee employee = new Employee(101, "adarsh", "adarsh@"+System.currentTimeMillis(), "IT");
         session.save(employee);
         transaction.commit();
-        logger.info("method executed sucessfully ");
+        logger.info("method executed successfully ");
     }
 }
