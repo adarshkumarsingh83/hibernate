6ea2d75b4962244caa74bb7e9d414ecb99406a34
adarshkumarsingh83 <adarshkumarsingh83@gmail.com>
2021-05-18 13:47:24 -0500

    HQLHibernatePartialObjectSelectQuery updated to hibernate 5

---
+++ HQLHibernatePartialObjectSelectQuery/src/main/java/com/adarsh/util/SessionFactoryUtil.java
@@ -0,0 +1,15 @@
+package com.adarsh.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionFactoryUtil {
+
+    static SessionFactory sessionFactoryObject=null;
+    public static SessionFactory getHibernateSession(){
+        if(sessionFactoryObject == null){
+            sessionFactoryObject=new Configuration().configure("hibernate-cfg.xml").buildSessionFactory();
+        }
+        return sessionFactoryObject;
+    }
+}
